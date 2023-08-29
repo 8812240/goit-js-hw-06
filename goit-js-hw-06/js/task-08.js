427b93f8d209446819963c5dffd2c0c5517224f2
Dawid <dawidun8@gmail.com>
2023-08-29 21:27:10 +0200

    done

--- goit-js-hw-06/js/task-08.js
+++ goit-js-hw-06/js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Zapobiegamy domyślnemu zachowaniu formularza (np. odświeżaniu strony)
+
+const emailInput = this.elements.email;
+const passwordInput = this.elements.password;
+
+if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Please fill in all fields.");
+} else {
+const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+    };
+
+    console.log(formData);
+    this.reset(); // Czyścimy wartości pól formularza
+}
+});
